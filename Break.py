837ffde08c26d46f67f2ac36b9e08a015ac0b235
GrayCanddle342 <alfredhpone@gmail.com>
2025-05-14 17:23:00 +0630

    Add files via upload

---
+++ Break.py
@@ -0,0 +1,11 @@
+#Take user input
+a = input ("Enter a word:")
+#program  to check Break keywoard 
+for i in a: #iterate the loop
+    if (i == 'A'): #condition 1
+    #display results
+     print ("A is found")
+     break #break statement
+    else:
+    #displey result
+     print ("A not foudd")
---
+++ Pass.py
@@ -0,0 +1,15 @@
+for x in range(10): #itrate the loop
+ if x % 20 ==0: 
+   print("twist")
+
+ elif x % 15 == 0:
+  pass
+ 
+ elif x % 15 == 0:
+  print("flizz")
+
+ elif x % 15 == 0:
+  print("buzz")
+
+else:
+ print(x) 
